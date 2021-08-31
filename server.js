dbbb1979b96853ddb7b9515950a49f3e5378881b
Buney <dpkinser93@gmail.com>
2021-08-31 17:56:41 -0600

    added server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect('mongodb://localhost:27017/workoutTracker', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+})
+    .then(() => console.log('Connected to db successfully'))
+    .catch(e => console.log(e));
